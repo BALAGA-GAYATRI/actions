30b3c9038f82bc5bddb9a1c4ec349f2c26c28f33
Balaga Gayatri <balaga-gayatri@github.com>
2021-07-19 19:23:47 +0530

    py

--- json2html.py
+++ json2html.py
@@ -1,6 +1,6 @@
 # import os
 # os.system('pip install json2table')
-import json2table
+import json2html
 import json
 
 f = open('./index.json', encoding='utf-8')
@@ -8,9 +8,10 @@ f = open('./index.json', encoding='utf-8')
 infoFromJson = json.load(f)
 build_direction = "LEFT_TO_RIGHT"
 table_attributes = {"style": "width:100%"}
-table = json2table.convert(infoFromJson, 
-                         build_direction=build_direction, 
-                         table_attributes=table_attributes)
+# table = json2table.convert(infoFromJson, 
+#                          build_direction=build_direction, 
+#                          table_attributes=table_attributes)
+table = json2html.convert(json = infoFromJson)
 f = open( 'docs/validation-result.html', 'w' )
 f.write( table )
 f.close()
